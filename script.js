47b6ba3fa8c43cd48c26a5b2cec60978f6a1c314
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-05-13 21:42:26 -0700

    Completed Code

--- script.js
+++ script.js
@@ -4,65 +4,62 @@ let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYXZ";
 let numbers = "0123456789";
 let allsymbols = "!@#$%^&*()_+~";
 
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
   passwordText.value = password;
-  console.log();
-}
+};
 
-// if ((parseInt(passwordLength) >= 8) && (parseInt(passwordLength) <= 128)) {
-//   // return passwordLength;
-// } else {
-//   askLength()
-// }
 
 function askLength() {
   let passwordLength = prompt("How many characters would you like your password to contain? Must be between 8-128.");
 
-  if (Number.isInteger(passwordLength)) {
+  if (Number.isInteger(parseInt(passwordLength))) {
     console.log(passwordLength);
     if ((passwordLength >= 8) && (passwordLength <= 128)) {
       return passwordLength;
-    }    
+    }
   } else {
     alert("Please enter a valid length!")
     askLength();
   }
 }
 
-  function generatePassword() {
-    let useUppercase = confirm("Use uppercase?");
-    let useLowercase = confirm("Use lowercase?");
-    let useSymbols = confirm("Use symbols?");
-    let useIntegers = confirm("Use integers?");
-    let length = askLength();
-    let useableCharacters = returnUseableCharacters(useUppercase, useLowercase, useSymbols, useIntegers);
-    // is it a number
-    // if it is how do I check it against me parameters
-    // if that's all true then what
-    // } RETURNING ENTIRE COMBINATION OF STRINGS
+function generatePassword() {
+  let useUppercase = confirm("Use uppercase?");
+  let useLowercase = confirm("Use lowercase?");
+  let useSymbols = confirm("Use symbols?");
+  let useIntegers = confirm("Use integers?");
+  const length = askLength();
+  let useableCharacters = returnUseableCharacters(useUppercase, useLowercase, useSymbols, useIntegers);
+  let passwordString = "";
+  let i = 0;
+  while (i < length) {
+    let randomCharacter = useableCharacters[Math.floor(Math.random() * useableCharacters.length)];
+    passwordString += randomCharacter;
+    console.log(randomCharacter);
+    i++;
   }
+  console.log(passwordString);
+  return passwordString;
+}
 
 
-  function returnUseableCharacters(upper, lower, symbols, numeric) {
-    let characters = "";
-    if (upper) {
-      characters += uppercase
-    }
-    if (lower) {
-      characters += lowercase
-    }
-    if (symbols) {
-      characters += allsymbols
-    }
-    if (numeric) {
-      characters += numbers
-    }
-    console.log(characters);
-    return characters;
-
-  } 
+function returnUseableCharacters(upper, lower, symbols, numeric) {
+  let characters = "";
+  if (upper) {
+    characters += uppercase
+  }
+  if (lower) {
+    characters += lowercase
+  }
+  if (symbols) {
+    characters += allsymbols
+  }
+  if (numeric) {
+    characters += numbers
+  }
+  return characters;
+};
